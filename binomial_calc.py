be0bcef101c66333a8391f80b1ad6833e8151fba
aryanmahajan16 <aryan.mahajan1807@gmail.com>
2025-09-01 14:54:43 +0530

    Update binomial_calc.py

--- binomial_calc.py
+++ binomial_calc.py
@@ -1,4 +1,6 @@
-import definations as df 
+import definations as df # df.combn(n,r) = combination calculator, nCr
+#df.fact(n) = product of first n natural numbers
+
 print("For the equation (a + b)^n")
 config = input("Type a if \"a\" is variable, \"b\" if b is variable, \"ab\" if both are variable or \"pass\" if none are variable: ")
 n = int(input("Enter the power of the expression: "))
@@ -12,7 +14,7 @@ if config.lower() == "a":
         coeff = df.combn(n, i)
         pwr_a = n - i
         pwr_b = i
-        term = f"{coeff}*{a}^{pwr_a}" if pwr_a > 0 else f"{coeff}"
+        term = f"{coeff*(b**pwr_b)}*{a}^{pwr_a}" if pwr_a > 0 else f"{coeff}"
         if pwr_b > 0 and pwr_b != 1:
             term += f"*{b**pwr_b}"
         b4calc += term + " + "
@@ -27,9 +29,10 @@ elif config.lower() == "b":
         coeff = df.combn(n, i)
         pwr_a = i
         pwr_b = n - i
+        term = ""
         if pwr_a > 0 and pwr_a != 1:
             term += f"*{a**pwr_a}"
-        term = f"{coeff}*{b}^{pwr_b}" if pwr_b > 0 else f"{coeff}"
+        term = f"{coeff*(a**pwr_a)}*{b}^{pwr_b}" if pwr_b > 0 else f"{coeff}"
         b4calc += term + " + "
     b4calc = b4calc.strip(" + ")
     print(b4calc) 
@@ -37,12 +40,13 @@ elif config.lower() == "ab":
     for i in range(n+1):  
         coeff = df.combn(n, i)
         pwr_a = n - i
-        pwr_b = n - i
-        term = f"{coeff}*{a}^{pwr_a}" if pwr_a > 0 else f"{coeff}"
-        term += f"{coeff}*{b}^{pwr_b}" if pwr_b > 0 else f"{coeff}"
+        pwr_b = i
+        term = f"{coeff}*{a}^{pwr_a}*{b}^{pwr_b}"
         b4calc += term + " + "
-    b4calc = b4calc.strip
+    b4calc = b4calc.strip(" + ")
     print(b4calc)
 else:
-    print("Wrong Input!")
+    print("Invalid Input!")
+
 input("Press any key to exit...")
+
